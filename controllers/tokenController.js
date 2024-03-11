6417a97c1a87a17ab281076ea306dd56c9dab490
Uriel Badillo Garrido <urielbadillogarrido@MacBook-Air-de-Uriel.local>
2024-03-10 19:19:53 -0600

    actuailzacion puerto

--- controllers/tokenController.js
+++ controllers/tokenController.js
@@ -4,7 +4,7 @@ exports.getToken = (req, res) => {
     // La clave secreta debería ser más compleja y almacenada de manera segura para producción
     const secretKey = 'tuClaveSecreta';
     const options = { expiresIn: '1h' }; // El token expira en 1 hora
-    const payload = { data: 'dataGenerica' }; // Payload del token, ajusta a tus necesidades
+    const payload = { data: 'dataGenerica' }; // Payload del token
 
     const token = jwt.sign(payload, secretKey, options);
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const tokenRoutes = require('./routes/tokenRoutes');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
 app.use(express.json());
 app.use('/api', tokenRoutes);
